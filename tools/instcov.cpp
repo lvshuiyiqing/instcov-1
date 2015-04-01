e6ff30c1372da4a422f8956cd3ecfb56c1b33133
Zhiqiang Zhang <xxyzzzq@gmail.com>
2015-04-01 14:21:36 +0800

    first working version

---
+++ tools/instcov.cpp
@@ -0,0 +1,142 @@
+//===-- instcov.cpp - Instruction class definition --------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// \brief This file contains the FrontendAction for InstCov
+///
+//===----------------------------------------------------------------------===//
+#include <cstdio>
+#include <memory>
+#include <string>
+#include <sstream>
+
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/ASTConsumers.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace clang;
+using namespace clang::tooling;
+
+static llvm::cl::OptionCategory ToolingInstCovCategory("InstCov Category");
+
+// By implementing RecursiveASTVisitor, we can specify which AST nodes
+// we're interested in by overriding relevant methods.
+class MyASTVisitor : public RecursiveASTVisitor<MyASTVisitor> {
+public:
+  MyASTVisitor(Rewriter &R) : TheRewriter(R) {}
+
+  bool VisitStmt(Stmt *s) {
+    // Only care about If statements.
+    if (isa<IfStmt>(s)) {
+      IfStmt *IfStatement = cast<IfStmt>(s);
+      Stmt *Then = IfStatement->getThen();
+
+      TheRewriter.InsertText(Then->getLocStart(), "// the 'if' part\n", true,
+                             true);
+
+      Stmt *Else = IfStatement->getElse();
+      if (Else)
+        TheRewriter.InsertText(Else->getLocStart(), "// the 'else' part\n",
+                               true, true);
+    }
+
+    return true;
+  }
+
+  bool VisitFunctionDecl(FunctionDecl *f) {
+    // Only function definitions (with bodies), not declarations.
+    if (f->hasBody()) {
+      Stmt *FuncBody = f->getBody();
+
+      // Type name as string
+      QualType QT = f->getReturnType();
+      std::string TypeStr = QT.getAsString();
+
+      // Function name
+      DeclarationName DeclName = f->getNameInfo().getName();
+      std::string FuncName = DeclName.getAsString();
+
+      // Add comment before
+      std::stringstream SSBefore;
+      SSBefore << "// Begin function " << FuncName << " returning " << TypeStr
+               << "\n";
+      SourceLocation ST = f->getSourceRange().getBegin();
+      TheRewriter.InsertText(ST, SSBefore.str(), true, true);
+
+      // And after
+      std::stringstream SSAfter;
+      SSAfter << "\n// End function " << FuncName;
+      ST = FuncBody->getLocEnd().getLocWithOffset(1);
+      TheRewriter.InsertText(ST, SSAfter.str(), true, true);
+    }
+
+    return true;
+  }
+
+private:
+  Rewriter &TheRewriter;
+};
+
+// Implementation of the ASTConsumer interface for reading an AST produced
+// by the Clang parser.
+class InstCovASTConsumer : public ASTConsumer {
+ public:
+  InstCovASTConsumer(Rewriter &R) : Visitor(R) {}
+
+  // Override the method that gets called for each parsed top-level
+  // declaration.
+  virtual bool HandleTopLevelDecl(DeclGroupRef DR) {
+    for (DeclGroupRef::iterator b = DR.begin(), e = DR.end(); b != e; ++b)
+      // Traverse the declaration using our AST visitor.
+      Visitor.TraverseDecl(*b);
+    return true;
+  }
+
+private:
+  MyASTVisitor Visitor;
+};
+
+class InstCovAction : public ASTFrontendAction {
+ public:
+  virtual void EndSourceFileAction() override {
+    SourceManager &SM = TheRewriter.getSourceMgr();
+    llvm::errs() << "** EndSourceFileAction for: "
+                 << SM.getFileEntryForID(SM.getMainFileID())->getName() << "\n";
+
+    TheRewriter.getEditBuffer(SM.getMainFileID()).write(llvm::outs());
+  }
+
+  virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
+      clang::CompilerInstance &Compiler, llvm::StringRef InFile) override {
+    llvm::errs() << "** Creating AST consumer for: " << InFile << "\n";
+    TheRewriter.setSourceMgr(Compiler.getSourceManager(), Compiler.getLangOpts());
+    return std::unique_ptr<clang::ASTConsumer>(
+        new InstCovASTConsumer(TheRewriter));
+  }
+ private:
+  Rewriter TheRewriter;
+};
+
+int main(int argc, const char **argv) {
+  CommonOptionsParser OptionsParser(argc, argv, ToolingInstCovCategory);
+  ClangTool Tool(OptionsParser.getCompilations(),
+                 OptionsParser.getSourcePathList());
+
+  
+  Tool.run(newFrontendActionFactory<InstCovAction>().get());
+  return 0;
+}
+
