5bb96d940f6c230d62dc5db8248e55d6fd571ba0
Zhiqiang Zhang <xxyzzzq@gmail.com>
2015-04-02 22:35:07 +0800

    working for BC, still bug for recursively inserting

--- tools/InstCovASTVisitor.cpp
+++ tools/InstCovASTVisitor.cpp
@@ -14,6 +14,7 @@
 
 #include <sstream>
 
+#include "clang/Lex/Lexer.h"
 #include "clang/AST/AST.h"
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/RecursiveASTVisitor.h"
@@ -29,18 +30,64 @@
 using namespace clang;
 
 namespace{
+
+  SourceLocation findSemiAfterLocation(Rewriter &R, SourceLocation sl) {
+    llvm::errs() << "finding semicolon after location\n";
+    SourceManager &SM = R.getSourceMgr();
+    if (sl.isMacroID()) {
+      // in a macro, return invalid
+      llvm::errs() << "ERR: the given location is inside a macro\n";
+      return SourceLocation();
+    }
+    sl = Lexer::getLocForEndOfToken(sl, 0, SM, R.getLangOpts());
+    std::pair<FileID, unsigned> locInfo = SM.getDecomposedLoc(sl);
+
+    // Try to load the file buffer
+    bool invalidTemp = false;
+    StringRef file = SM.getBufferData(locInfo.first, &invalidTemp);
+    if (invalidTemp) {
+      llvm::errs() << "ERR: cannot find the original location\n";
+      return SourceLocation();
+    }
+    
+    const char *tokenBegin = file.data() + locInfo.second;
+
+    Lexer lexer(SM.getLocForStartOfFile(locInfo.first),
+                R.getLangOpts(),
+                file.begin(), tokenBegin, file.end());
+    Token tok;
+    lexer.LexFromRawLexer(tok);
+    if (tok.isNot(tok::semi)) {
+      llvm::errs() << "ERR: the next location is not a semicolon\n";
+      return SourceLocation();
+    }
+    llvm::errs() << "SUCCESS: semicolon found\n";
+    return tok.getLocation();
+  }
+
+
+  SourceLocation FindEndLoc(Stmt *s, Rewriter &R) {
+    SourceManager &SM = R.getSourceMgr();
+    SourceLocation EndSL = s->getLocEnd();
+    char TailChar = *SM.getCharacterData(EndSL);
+    if (TailChar == '}') {
+      return EndSL.getLocWithOffset(1);
+    }
+    return findSemiAfterLocation(R, EndSL).getLocWithOffset(1);
+  }
+  
   void InstCompoundStmt(CompoundStmt *s, Rewriter &R,
                         uint64_t id, uint64_t bid) {
     std::stringstream ss;
-    ss << "Dump(" << id << ", " << bid << ");\n";
+    ss << "\nDump(" << id << ", " << bid << ");\n";
     R.InsertText(s->getLBracLoc().getLocWithOffset(1), ss.str(), true, true);
   }
 
   void InstSingleStmt(Stmt *s, Rewriter &R, uint64_t id, uint64_t bid) {
-    R.InsertText(s->getLocStart(), "{\n", 0, 0);
     std::stringstream ss;
-    ss << "\nDump(" << id << ", " << bid << ");\n}\n";
-    R.InsertText(s->getLocEnd(), ss.str(), true, true);
+    ss << "{\nDump(" << id << ", " << bid << ");\n";
+    R.InsertText(s->getLocStart(), ss.str(), 0, 0);
+    R.InsertText(FindEndLoc(s, R), "\n}\n", true, true);
   }
 
   void InstInBlock(Stmt *s, Rewriter &R, uint64_t id, uint64_t bid) {
