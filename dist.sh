3f53569de181889bce2c512984dbbcfe4161d317
Zhiqiang Zhang <xxyzzzq@gmail.com>
2015-04-08 16:23:26 +0800

    fixed dist.sh

--- dist.sh
+++ dist.sh
@@ -3,9 +3,11 @@
 rm -rf instcov-dist
 
 mkdir -p instcov-dist
+mkdir -p instcov-dist/lib
+mkdir -p instcov-dist/bin
 
-cp ../build/bin/instcov instcov-dist
-cp ../build/lib/libinstcov_rt.a instcov-dist
+cp ../build/bin/instcov instcov-dist/bin
+cp ../build/lib/libinstcov_rt.a instcov-dist/lib
 cp -r ./runtime/include/ instcov-dist
 cp -r ./tests instcov-dist
 cp -r ./scripts instcov-dist
