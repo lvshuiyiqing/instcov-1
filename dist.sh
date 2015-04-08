5844f4130e8a5006f8d27449dd41e5bd83682808
Zhiqiang Zhang <xxyzzzq@gmail.com>
2015-04-08 15:59:31 +0800

    fixed dist.sh

--- dist.sh
+++ dist.sh
@@ -7,4 +7,7 @@ mkdir -p instcov-dist
 cp ../build/bin/instcov instcov-dist
 cp ../build/lib/libinstcov_rt.a instcov-dist
 cp ./runtime/include/ instcov-dist -r
+cp ./tests/ccwraper.sh instcov-dist
+cp ./tests/instcov.sh instcov-dist
+
 tar -czf instcov-dist.tar.gz instcov-dist
