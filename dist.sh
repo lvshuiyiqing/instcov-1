b11bf852809c61021523a4b8a2d8cf0d4f168033
Zhiqiang Zhang <xxyzzzq@gmail.com>
2015-04-08 16:21:50 +0800

    added readme and fixed dist.sh

--- dist.sh
+++ dist.sh
@@ -6,8 +6,9 @@ mkdir -p instcov-dist
 
 cp ../build/bin/instcov instcov-dist
 cp ../build/lib/libinstcov_rt.a instcov-dist
-cp ./runtime/include/ instcov-dist -r
-cp ./tests/ccwraper.sh instcov-dist
-cp ./tests/instcov.sh instcov-dist
+cp -r ./runtime/include/ instcov-dist
+cp -r ./tests instcov-dist
+cp -r ./scripts instcov-dist
+cp README instcov-dist
 
 tar -czf instcov-dist.tar.gz instcov-dist
