d8e2c33db291d3320b1a8fe5173cb5de285292da
Zhiqiang Zhang <xxyzzzq@gmail.com>
2015-04-20 20:13:59 +0800

    now no need to include runtime header in preprocessing

--- scripts/instcov.sh
+++ scripts/instcov.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-/Users/zzq/llvm/3.6/build/bin/instcov $@
+INSTCOV_RT_PATH=/Users/zzq/llvm/3.6/instcov/runtime/include/
 
+/Users/zzq/llvm/3.6/build/bin/instcov \
+    $@ -- clang -I $INSTCOV_RT_PATH \
+    -include instcov_rt.h
