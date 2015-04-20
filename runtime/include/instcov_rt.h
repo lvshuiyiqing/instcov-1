32053b18e34cb205ec36019317e8aeb81a68c498
Zhiqiang Zhang <xxyzzzq@gmail.com>
2015-04-20 17:20:03 +0800

    fixed runtime header. modified ccwraper.sh, the user should include header in preprocessing

--- runtime/include/instcov_rt.h
+++ runtime/include/instcov_rt.h
@@ -12,12 +12,21 @@
 ///
 //===----------------------------------------------------------------------===//
 
+#ifndef INSTCOV_RT_H_
+#define INSTCOV_RT_H_
+
 #ifdef __cplusplus
 #include <cstdint>
 #else  // __cplusplus
 #include <stdint.h>
 #endif  // __cplusplus
 
+#ifdef __cplusplus
 extern "C" {
+#endif  // __cplusplus
   void instcov_dump(uint64_t id_high, uint64_t id_low, uint64_t bid);
+#ifdef __cplusplus
 }
+#endif  // __cplusplus
+
+#endif  // INSTCOV_RT_H_
