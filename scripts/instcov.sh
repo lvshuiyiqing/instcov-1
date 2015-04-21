2dc9054ef8cad3b837b6a19911ff4acf511804c9
Zhiqiang Zhang <xxyzzzq@gmail.com>
2015-04-21 13:02:32 +0800

    some minor fixes, added run_single_file.sh

--- scripts/instcov.sh
+++ scripts/instcov.sh
@@ -2,8 +2,5 @@
 
 INSTCOV_RT_PATH=/Users/zzq/llvm/3.6/instcov/runtime/include/
 
-echo /Users/zzq/llvm/3.6/build/bin/instcov \
-    $@ -- clang -I $INSTCOV_RT_PATH
-
 /Users/zzq/llvm/3.6/build/bin/instcov \
     $@ -- clang -I $INSTCOV_RT_PATH
---
+++ scripts/run_single_file.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 1 ]; then
+    echo "syntax: <path>/run_single_file.sh <file> [args ...]"
+    exit 1
+fi
+
+rpath=`dirname $0`
+
+file=$1
+extension="${file##*.}"
+filename="${file%.*}"
+
+shift 1
+args=$@
+
+${rpath}/prep.sh $file
+${rpath}/instcov.sh -inst-conditions ${filename}.i.${extension}
+${rpath}/ccwraper.sh ${filename}.i.trans.${extension} -o ${filename}
+echo "executing ./${filename} $args"
+./${filename} $args
+${rpath}/instcov-view.sh dump.instcov ${filename}.i.${extension}.dbginfo -o ${filename}.pt
