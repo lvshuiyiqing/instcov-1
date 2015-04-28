a64cf792c371d41d26997a92b089b7342f2f15a1
Zhiqiang Zhang <xxyzzzq@gmail.com>
2015-04-28 14:54:10 +0800

    updated dist.sh

--- dist.sh
+++ dist.sh
@@ -9,12 +9,16 @@ mkdir -p instcov-dist
 mkdir -p instcov-dist/lib
 mkdir -p instcov-dist/bin
 
+markdown README.md > README.html
+markdown doc/InstCovImpl.md > doc/InstCovImpl.html
+
 cp ../build/bin/instcov instcov-dist/bin
 cp ../build/bin/instcov-view instcov-dist/bin
 cp ../build/lib/libinstcov_rt.a instcov-dist/lib
 cp -r ./runtime/include/ instcov-dist
 cp -r ./tests instcov-dist
 cp -r ./scripts instcov-dist
-cp README.md instcov-dist
+cp README.md README.html instcov-dist
+cp -r doc instcov-dist
 
 tar -czf instcov-dist_${VER}_${PLATFORM}_${MACHINE}.tar.gz instcov-dist
