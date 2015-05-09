3d4c7981ef1518315254e4e0aadf1d37a39871e5
Zhiqiang Zhang <xxyzzzq@gmail.com>
2015-05-09 21:10:24 +0800

    MCDCScan

---
+++ tools/mcdc-scan/LogMgr.cpp
@@ -0,0 +1,110 @@
+//===-- LogMgr.cpp ------ LogMgr class method definitions -------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// \brief This file contains the method definitions of LogMgr
+///
+//===----------------------------------------------------------------------===//
+
+#include <tuple>
+#inlcude <fstream>
+#include <iostream>
+#include "LogMgr.h"
+
+using namespace instcov;
+
+namespace {
+void eatOrQuit(std::istream &In, const std::string &str) {
+  for (auto it = str.begin(), ie = str.end(); it != ie; ++it) {
+    if (In.get() != *it) {
+      std::cerr << "error matching: " << str << std::endl;
+      exit(1);
+    }
+    if (!In) {
+      std::cerr << "error matching: " << str << std::endl;
+      exit(1);
+    }
+  }
+}
+
+std::tuple<unsigned, UUID, uint64_t> parseLine(std::istream &In, bool &success) {
+  unsinged depth = 0;
+  UUID Uuid;
+  uint64_t Bid = 0;
+  std::string line;
+  if (!std::getline(InFile, line)) {
+    success = false;  // last line
+    return;
+  }
+  std::stringstream ss;
+  while (ss.peek() == '-') {
+    ++depth;
+    ss.get();
+  }
+  ss.peek();
+  if (ss.eof() || !ss) {
+    std::cerr << "bad log\n";
+    exit(1);
+  }
+  std::string strUUID;
+  getline(ss, strUUID);
+  Uuid = parseString(strUUID);
+  eatOrQuit(ss, ":");
+  ss >> Bid;
+  return std::make_tuple(depth, Uuid, Bid);
+}
+}
+
+LogMgr::LogMgr() {
+}
+
+LogMgr::~LogMgr() {
+}
+
+void LogMgr::loadFile(const std::string &fileName) {
+  std::ifstream InFile(fileName.c_str());
+  if (!InFile) {
+    std::cerr << "cannot open file: " << fileName << std::endl;
+    exit(1);
+  }
+
+  FileNames.push_back(fileName);
+  std::stack<std::vector<std::pair<UUID, uint64_t> > > S;
+  S.push(std::vector<std::pair<UUID, uint64_t> >());
+  bool success = true;
+  while (auto PL = parseLine(InFile, success)) {
+    if (!success) {
+      break;
+    }
+    unsigned depth = 0;
+    UUID Uuid;
+    uint64_t Bid;
+    std::tie(depth, Uuid, Bid) = PL;
+    if (depth+1 == S.size()) {
+      PL.top().push_back(std::makr_pair(Uuid, Bid));
+      PL.push(std::vector<std::pair<UUID, uint64_t>());
+      continue;
+    }
+    while (depth < S.size()) {
+      if (S.top().empty()){
+        S.pop();
+      } else {
+        LogEntry Entry;
+        Entry.FID = FileNames.size();
+        Entry.RID = LogEntries.size();
+        Entry.Conditions.assign(S.top().begin(), S.top().end());
+        S.pop();
+        Entry.Decision = S.top.back();
+        LogEntries.push_back(Entry);
+        Children[Entry.Decision.first].insert(
+            Entry.Conditions.begin(), Entry.Conditions.end());
+      }
+    }
+  }
+}
---
+++ tools/mcdc-scan/MCDCScan.cpp
@@ -0,0 +1,14 @@
+//===-- MCDCScan.cpp --------- main file ------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// \brief This file contains the basic infrastructures for mcdc-scan
+///
+//===----------------------------------------------------------------------===//
+
