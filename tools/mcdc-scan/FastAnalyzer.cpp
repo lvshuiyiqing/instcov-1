6ec3d2b5dcb0921e5c61abe1abde22ba5b5076e0
Zhiqiang Zhang <xxyzzzq@gmail.com>
2015-06-14 22:47:23 +0800

    more code for SCAnalyzer

--- tools/mcdc-scan/FastAnalyzer.cpp
+++ tools/mcdc-scan/FastAnalyzer.cpp
@@ -18,16 +18,10 @@
 #include "llvm/Support/CommandLine.h"
 
 using namespace llvm;
-cl::opt<bool> CountsOnly("counts-only",
-                         cl::desc("only counts"),
-                         cl::init(false));
-
-cl::opt<bool> Verbose(
-    "v",
-    cl::desc("dump more verbosely"),
-    cl::init(false));
-
 using namespace instcov;
+
+extern cl::opt<bool> CountsOnly;
+extern cl::opt<bool> Verbose;
 
 void FastAnalyzer::registerEntry(const LogEntry *entry) {
   std::vector<bool> bits;
@@ -62,38 +56,6 @@ void FastAnalyzer::registerEntry(const LogEntry *entry) {
 }
 
 void FastAnalyzer::finalize(void) {
-}
-
-namespace {
-std::string getLocString(const LogMgr &LM, UUID_t Uuid) {
-  if (LM.getLocInfos().count(Uuid)) {
-    return LM.getLocInfos().find(Uuid)->second.toString();
-  }
-  return "NA";
-}
-
-struct LocSorter {
- public:
-  LocSorter(const LogMgr &lm)
-      : LM(lm) {}
-
-  template<typename T>
-  bool operator()(const T &LHS, const T &RHS) const {
-    if (LM.getLocInfos().count(LHS->first) == 0) {
-      return true;
-    }
-    if (LM.getLocInfos().count(RHS->first) == 0) {
-      return false;
-    }
-    const LocInfo &LHS_LI = LM.getLocInfos().find(LHS->first)->second;
-    const LocInfo &RHS_LI = LM.getLocInfos().find(RHS->first)->second;
-    return std::make_tuple(LHS_LI.FileName, LHS_LI.Line, LHS_LI.Col) <
-      std::make_tuple(RHS_LI.FileName, RHS_LI.Line, RHS_LI.Col);
-  }
-  
- private:
-  const LogMgr &LM;
-};
 }
 
 std::vector<FastAnalyzer::Data_t::const_iterator>
--- tools/mcdc-scan/MCDCAnalyzer.h
+++ tools/mcdc-scan/MCDCAnalyzer.h
@@ -23,6 +23,37 @@ class MCDCAnalyzer {
   virtual void registerEntry(const LogEntry *entry) = 0;
   virtual void dump(std::ostream &OS, const LogMgr &LM) const = 0;
   virtual void finalize(void) = 0;
+
+ public:
+  static std::string getLocString(const LogMgr &LM, UUID_t Uuid) {
+    if (LM.getLocInfos().count(Uuid)) {
+      return LM.getLocInfos().find(Uuid)->second.toString();
+    }
+    return "NA";
+  }
+
+  struct LocSorter {
+   public:
+    LocSorter(const LogMgr &lm)
+        : LM(lm) {}
+    
+    template<typename T>
+    bool operator()(const T &LHS, const T &RHS) const {
+      if (LM.getLocInfos().count(LHS->first) == 0) {
+        return true;
+      }
+      if (LM.getLocInfos().count(RHS->first) == 0) {
+        return false;
+      }
+      const LocInfo &LHS_LI = LM.getLocInfos().find(LHS->first)->second;
+      const LocInfo &RHS_LI = LM.getLocInfos().find(RHS->first)->second;
+      return std::make_tuple(LHS_LI.FileName, LHS_LI.Line, LHS_LI.Col) <
+      std::make_tuple(RHS_LI.FileName, RHS_LI.Line, RHS_LI.Col);
+    }
+    
+ private:
+  const LogMgr &LM;
+};
 };
 }
 
