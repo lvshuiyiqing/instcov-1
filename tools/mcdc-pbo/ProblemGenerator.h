fa24d4c1fe1ac9570e3767bcb669b290fa09112e
Zhiqiang Zhang <xxyzzzq@gmail.com>
2015-06-22 22:00:42 +0800

    more code for mcdc-pbo

--- tools/mcdc-pbo/ProblemGenerator.h
+++ tools/mcdc-pbo/ProblemGenerator.h
@@ -15,7 +15,36 @@
 #ifndef INSTCOV_PROBLEMGENERATOR_H_
 #define INSTCOV_PROBLEMGENERATOR_H_
 
+#include <map>
+#include "instcov/LogMgr.h"
+#include "llvm/ADT/StringMap.h"
+
 namespace instcov {
+class ProblemGenerator {
+ public:
+  ProblemGenerator(void);
+  ~ProblemGenerator(void);
+
+  void registerLogEntry(const LogEntry *E);
+
+ private:
+  // decision / conditions
+  std::size_t encodeDC(
+      std::size_t sid,
+      std::size_t fid, std::size_t rid,
+      char type);
+  // test / visit ID
+  std::size_t encodeTV(std::size_t id, char type);
+
+  std::size_t encodeStr(const std::string &str);
+  
+ private:
+  std::map<UUID_t, const LogEntry *> Uuid2LogEntries;
+  std::map<UUID_t, std::size_t> Uuid2SID;
+  std::vector<UUID_t> SID2Uuid;
+  llvm::StringMap<std::size_t> IDPool;
+  std::vector<std::string> ID2Str;
+};
 }
 
 #endif  // INSTCOV_PROBLEMGENERATOR_H_
