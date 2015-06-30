3a06fe6202943af0c69c89b0297dd2f30706b346
Zhiqiang Zhang <xxyzzzq@gmail.com>
2015-06-30 16:27:18 +0800

    removed old bash scripts

--- scripts/ccwraper.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rpath=`dirname $0`
-source ${rpath}/instcov_env
-
-$INSTCOV_CC "$@" -L${INSTCOV_LIBRARY_PATH} -linstcov_rt -lstdc++
--- scripts/instcov-view.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rpath=`dirname $0`
-source ${rpath}/instcov_env
-
-${INSTCOV_BIN_PATH}/instcov-view "$@"
